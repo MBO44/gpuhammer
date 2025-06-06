9753b65c0364a4a3c5ebfb73aa65757424f76f8f
sushidoggg <joycequ04@gmail.com>
2025-06-05 23:03:59 -0400

    updated trr sampler size script

--- run_fig12.sh
+++ run_fig12.sh
@@ -14,19 +14,12 @@ num_rows=64169      # Number of rows in the row_set (line number - 1)
 vic_pat=55          # Victim row data pattern in hex
 agg_pat=aa          # Aggressor row data pattern in hex
 
-aggs=(24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8)
-warps=(8  8  8  8 10 10  9  9  8  8  7  7  6  6 10 10 8)
-threads=(3 3 3  3  2  2  2  2  2  2  2  2  2  2  1 1 1)
-delays=(56 56 56 56 56 56 56 56 56 56 56 56 56 56 56 56 56 56)
+num_warp=8
+num_thread=3
+delay=56
 
 # for ((i=0; i<16; i++)); do
-  num_agg=8
-  # num_warp=${warps[$i]}
-  # num_thread=${threads[$i]}
-  # delay=${delays[$i]}
-  num_warp=8
-  num_thread=3
-  delay=56
+for num_agg in {8..24}; do
   
   # File paths
   mkdir -p "$HAMMER_ROOT/results/fig12/raw_data"
@@ -39,4 +32,4 @@ delays=(56 56 56 56 56 56 56 56 56 56 56 56 56 56 56 56 56 56)
   $HAMMER_ROOT/src/out/build/trr_sampler $rowset_file $((num_agg - 1)) $addr_step $iterations $agg_rowid $row_step $mem_size $num_warp $num_thread $delay 1 $count_iter $num_rows $vic_pat $agg_pat $bitflip_file > $log_file
 
   sleep 5
-# done
+done
